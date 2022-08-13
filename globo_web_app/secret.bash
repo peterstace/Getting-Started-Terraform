95c54e9d546b618e422120057f7d64460343ea23
Peter Stace <peterstace@gmail.com>
2022-08-14 06:46:10 +1000

    Don't use variables for AWS creds

--- globo_web_app/secret.bash
+++ globo_web_app/secret.bash
@@ -1,2 +1,2 @@
-export TF_VAR_aws_access_key="$(agepass show personal/aws/access_keys | head -n1 | cut -d' ' -f4)" 
-export TF_VAR_aws_secret_key="$(agepass show personal/aws/access_keys | tail -n1 | cut -d' ' -f4)"
+export AWS_ACCESS_KEY="$(agepass show personal/aws/access_keys | head -n1 | cut -d' ' -f4)"
+export AWS_SECRET_KEY="$(agepass show personal/aws/access_keys | tail -n1 | cut -d' ' -f4)"
