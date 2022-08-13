d0bccfba8a1fc137d2ca6308cdcd9c21b27dc032
Peter Stace <peterstace@gmail.com>
2022-08-13 19:45:16 +1000

    Use ALB

--- globo_web_app/loadbalance.tf
+++ globo_web_app/loadbalance.tf
@@ -12,7 +12,38 @@ resource "aws_lb" "nginx" {
 }
 
 ## aws_lb_target_group
+resource "aws_lb_target_group" "nginx" {
+  name     = "nginx-alb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+
+  tags = local.common_tags
+}
 
 ## aws_lb_listener
+resource "aws_lb_listener" "nginx" {
+  load_balancer_arn = aws_lb.nginx.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx.arn
+  }
+
+  tags = local.common_tags
+}
 
 ## aws_lb_target_group_attachment
+resource "aws_lb_target_group_attachment" "nginx1" {
+  target_group_arn = aws_lb_target_group.nginx.arn
+  target_id        = aws_instance.nginx1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "nginx2" {
+  target_group_arn = aws_lb_target_group.nginx.arn
+  target_id        = aws_instance.nginx2.id
+  port             = 80
+}
